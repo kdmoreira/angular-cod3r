245676208ac1041ea8f8e1f929f8f40c52335ac8
Karina Moreira <kakatinosa@hotmail.com>
2022-04-22 19:40:07 -0300

    Bindings examples and event binding for navigation

---
+++ src/app/components/product/product-create/product-create.component.ts
@@ -0,0 +1,22 @@
+import { ConditionalExpr } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-create',
+  templateUrl: './product-create.component.html',
+  styleUrls: ['./product-create.component.css']
+})
+export class ProductCreateComponent implements OnInit {
+
+  propLegal = "qualquer";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  fazerAlgo(): void {
+    console.log('Fazendo algo!');
+  }
+
+}
