b39df15ee7226930b786ace25ee1e1e32113adea
Karina Moreira <kakatinosa@hotmail.com>
2022-04-22 20:09:05 -0300

    Product model and service

--- src/app/components/product/product-create/product-create.component.ts
+++ src/app/components/product/product-create/product-create.component.ts
@@ -1,5 +1,6 @@
-import { ConditionalExpr } from '@angular/compiler';
+import { ProductService } from './../product.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-create',
@@ -8,15 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCreateComponent implements OnInit {
 
-  propLegal = "qualquer";
-
-  constructor() { }
+  constructor(
+    private productService: ProductService,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
+    
   }
 
-  fazerAlgo(): void {
-    console.log('Fazendo algo!');
+  createProduct(): void {
+    this.productService.showMessage('Produto criado!');
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products']);
   }
 
 }
