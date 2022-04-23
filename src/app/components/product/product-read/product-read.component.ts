59eab86548f6a124cbbfb6f472dc69208bab418a
Karina Moreira <kakatinosa@hotmail.com>
2022-04-23 19:46:27 -0300

    Product table and locale

--- src/app/components/product/product-read/product-read.component.ts
+++ src/app/components/product/product-read/product-read.component.ts
@@ -10,6 +10,7 @@ import { ProductService } from '../product.service';
 export class ProductReadComponent implements OnInit {
 
   products: Product[] = [];
+  displayedColumns: string[] = ['id', 'name', 'price'];
 
   constructor(private productService: ProductService) { }
 
